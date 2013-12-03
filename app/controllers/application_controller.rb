446413c9f3421863b105f4a0d74d757109474f5e
MuiNV54 <nguyen.van.mui@framgia.com>
2013-12-03 14:01:02 +0700

    sign in,out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,8 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit! }
+    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name,
+    	:email, :password, :birthday)}
   end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,10 @@ ActiveRecord::Schema.define(version: 20131203020846) do
   add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
 
   create_table "users", force: true do |t|
+    t.string   "name",                                null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.datetime "birthday",                            null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
