c36c556c9625823ba0b42fc2b81709602b046941
quynhbv <quynhbv.hp@gmail.com>
2013-12-04 09:16:43 +0700

    Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,9 +5,11 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @comment = Comment.new params.require(:comment).permit :content, :status_id
-    if @comment.save
-      @comment.update_attributes user_id: current_user.id, status_id: params[:status_id]
+    if params[:comment]
+      @comment = Comment.new params.require(:comment).permit :content, :status_id
+      if @comment.save
+        @comment.update_attributes user_id: current_user.id, status_id: params[:status_id]
+      end
     end
     redirect_to :back
   end
